b00ba9f86b23429f2be3f9ea448c9738300802b9
icad <rhisjaddjvtr@gmail.com>
2023-07-05 00:49:43 +0700

    javascript susah bgt

---
+++ Day4/js/myproject.js
@@ -0,0 +1,36 @@
+let dataBlog = []
+
+function addBlog(event) {
+    event.preventDefault()
+
+    let project = document.getElementById(input-name).value
+    let startdate = document.getElementById(start-date).value
+    let enddate = document.getElementById(end-date).value
+    let description = document.getElementById(description).value
+    let nodejs = document.getElementById(input-nodejs).checked
+    let nextjs = document.getElementById(input-nextjs).checked
+    let reactjs = document.getElementById(input-reactjs).checked
+    let typescript = document.getElementById(input-typescript).checked
+    let image = document.getElementById(upload).files
+
+    image = URL.createObjectURL(image[0])
+
+    let blog = {
+        project,
+        startdate,
+        enddate,
+        description,
+        image,
+    }
+    
+    dataBlog.push(blog)
+    renderBlog()
+
+    console.log(dataBlog)
+}
+
+
+
+
+    
+
