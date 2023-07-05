f46f6cbcfa2ed0f0eff49f63fc2d5ad99c170934
icad <rhisjaddjvtr@gmail.com>
2023-07-05 11:59:47 +0700

    susahbgt

--- Day4/js/myproject.js
+++ Day4/js/myproject.js
@@ -1,17 +1,17 @@
 let dataBlog = []
 
-function addBlog(event) {
+let addBlog = (event) => {
     event.preventDefault()
 
-    let project = document.getElementById(input-name).value
-    let startdate = document.getElementById(start-date).value
-    let enddate = document.getElementById(end-date).value
-    let description = document.getElementById(description).value
-    let nodejs = document.getElementById(input-nodejs).checked
-    let nextjs = document.getElementById(input-nextjs).checked
-    let reactjs = document.getElementById(input-reactjs).checked
-    let typescript = document.getElementById(input-typescript).checked
-    let image = document.getElementById(upload).files
+    let project = document.getElementById("input-name").value
+    let startdate = document.getElementById("start-date").value
+    let enddate = document.getElementById("end-date").value
+    let description = document.getElementById("description").value
+    let nodejs = document.getElementById("input-nodejs").checked
+    let nextjs = document.getElementById("input-nextjs").checked
+    let reactjs = document.getElementById("input-reactjs").checked
+    let typescript = document.getElementById("input-typescript").checked
+    let image = document.getElementById("input-file").files
 
     image = URL.createObjectURL(image[0])
 
@@ -27,6 +27,7 @@ function addBlog(event) {
     renderBlog()
 
     console.log(dataBlog)
+    console.log(project)
 }
 
 
